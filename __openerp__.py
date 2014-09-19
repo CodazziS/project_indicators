391c2da788dbed49838f506da2c498c509edf227
stephane- <tempo@codazzi.fr>
2014-09-19 09:57:36 +0200

    Update openerp.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Project Indicators",
-    'author' : 'Stéphane Codazzi @ TeMPO-consulting',
+    'author' : 'Stéphane Codazzi @ TeMPO-Consulting',
     'category': 'Project',
     'description': """
 Project Indicators
